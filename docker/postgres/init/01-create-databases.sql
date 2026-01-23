51a4948680fc7104669bb802d366b028bb8da0b4
ibrahim çadırcı <ibrahimh.cadirci@gmail.com>
2026-01-23 23:33:09 +0300

    Edits create-datatabases file

--- docker/postgres/init/01-create-databases.sql
+++ docker/postgres/init/01-create-databases.sql
@@ -1,8 +1,4 @@
--- Bu script sadece container ilk kez ayağa kalkarken çalışır (data volume boşsa)
 
 CREATE DATABASE user_db;
 CREATE DATABASE order_db;
 CREATE DATABASE delivery_db;
-
--- İstersen notification için de:
--- CREATE DATABASE notification_db;
